92f1c62ed844599ec221e8931a3efcc5e7e44064
kiran <kiran.vanapalli123@gmail.com>
2023-04-17 11:13:38 +0530

    udpate one record

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -14,6 +14,7 @@ export class HomeComponent implements OnInit {
   submitted:boolean = false
   errorMessage: any;
 
+  dataList:any  = []
   constructor(private fb:FormBuilder,private api:WetherservicesService){
 
   }
@@ -44,15 +45,41 @@ export class HomeComponent implements OnInit {
     this.api.getWetherByCity(this.weatherForm.get('cityName')?.value).subscribe((res) =>
     {
       console.log(res);
-
       if(res.cod ===  '200')
       {
         this.list = res.list
-        console.log(this.list);
-
         this.loading = false
         this.showTable = true
         this.showNoRecords = false
+
+        this.list.forEach((element: any) => {
+          var a = element.dt_txt.split(" ");
+          var date = a[0];
+          console.log(this.dataList.length);
+
+          if(this.dataList.length > 0)
+          {
+            console.log(this.dataList);
+
+            let data = this.dataList.find((x:any) => x.date === date)
+            if(data == undefined)
+            {
+              let obj = {
+                date:date,
+                wetherInfo:element
+              }
+              this.dataList.push(obj)
+            }
+          }
+          else
+          {
+            let obj = {
+              date:date,
+              wetherInfo:element
+            }
+            this.dataList.push(obj)
+          }
+        });
       }
       else if(res.cod ===  '404')
       {
